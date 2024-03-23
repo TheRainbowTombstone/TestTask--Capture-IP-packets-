8105e2606e1efc7e8f4b0a17ae9ee437086e7525
TheRainboowTombstone <no.victim.no.crime451@gmail.com>
2024-03-23 19:08:28 +0300

    Adding try-catch blocks

--- cpp_program/src/main.cpp
+++ cpp_program/src/main.cpp
@@ -1,19 +1,29 @@
 #include "pcap_handler.h"
 
 int main(int argc, char* argv[]) {
+    try {
+        PcapHandler pcapHandler;
+        const char* max_packets = "0";
 
-    PcapHandler pcapHandler;
-    const char* max_packets = "0";
-
-    if (argc == 3) {
-        pcapHandler.process_packets(argv[1], argv[2], max_packets);
+        try {
+            if (argc == 3) {
+                pcapHandler.process_packets(argv[1], argv[2], max_packets);
+            }
+            else if (argc == 4) {
+                pcapHandler.process_packets(argv[1], argv[2], argv[3]);
+            }
+            else {
+                throw invalid_argument("Usage: <source> <output_file.csv> <max_packets>(optional)");
+            }
+        }
+        catch(const exception& e) {
+            cerr << "Error: " << e.what() << endl;
+            return 1;
+        }
+        return 0;
     }
-    else if (argc == 4) {
-        pcapHandler.process_packets(argv[1], argv[2], argv[3]);
-    }
-    else {
-        cerr << "Usage: " << argv[0] << " <source> <output_file.csv> <max_packets>(optional)" << endl;
+    catch (const exception& e) {
+        cerr << "Error: " << e.what() << endl;
         return 1;
     }
-    return 0;
 }
--- cpp_program/src/pcap_handler.cpp
+++ cpp_program/src/pcap_handler.cpp
@@ -7,140 +7,147 @@ PcapHandler::~PcapHandler() {}
 unordered_map<ThreadKey, Thread, ThreadKeyHash> PcapHandler::thread;
 
 void PcapHandler::packet_handler(u_char* user, const struct pcap_pkthdr* pkthdr, const u_char* packet) {
+    try {
+        const struct sniff_ethernet* ethernet; 
+        const struct sniff_ip* ip;
+        const struct sniff_tcp* tcp;
+        const struct sniff_udp* udp;
 
-    const struct sniff_ethernet* ethernet;
-    const struct sniff_ip* ip;
-    const struct sniff_tcp* tcp;
-    const struct sniff_udp* udp;
+        // Variables will contain information about the protocols
+        int size_ip;
+        int size_tcp;
+        int size_udp;
+        int size_payload;
 
-    // Variables will contain information about the protocols
-    int size_ip;
-    int size_tcp;
-    int size_udp;
-    int size_payload;
+        uint32_t src_ip;
+        uint32_t dest_ip;
+        uint16_t src_port;
+        uint16_t dest_port;
 
-    uint32_t src_ip;
-    uint32_t dest_ip;
-    uint16_t src_port;
-    uint16_t dest_port;
+        ethernet = (struct sniff_ethernet*)(packet);
+        ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
+        size_ip = sizeof(struct sniff_ip);
 
-    ethernet = (struct sniff_ethernet*)(packet);
-    ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
-    size_ip = sizeof(struct sniff_ip);
+        // Assignment of values 
+        if (ip->ip_p == IPPROTO_TCP) {
+            tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);
+            size_tcp = sizeof(struct sniff_tcp);
 
-    // Assignment of values 
-    if (ip->ip_p == IPPROTO_TCP) {
-        tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);
-        size_tcp = sizeof(struct sniff_tcp);
+            // Functions are used to convert numbers from network byte order to host byte order 
+            size_payload = ntohs(ip->ip_len) - (size_ip + size_tcp);
+            src_ip = ntohl(ip->ip_src.s_addr);
+            dest_ip = ntohl(ip->ip_dst.s_addr);
+            src_port = ntohs(tcp->th_sport);
+            dest_port = ntohs(tcp->th_dport);
+        }
+        else if (ip->ip_p == IPPROTO_UDP) {
+            udp = (struct sniff_udp*)(packet + SIZE_ETHERNET + size_ip);
+            size_udp = sizeof(struct sniff_udp);
 
-        // Functions are used to convert numbers from network byte order to host byte order 
-        size_payload = ntohs(ip->ip_len) - (size_ip + size_tcp);
-        src_ip = ntohl(ip->ip_src.s_addr);
-        dest_ip = ntohl(ip->ip_dst.s_addr);
-        src_port = ntohs(tcp->th_sport);
-        dest_port = ntohs(tcp->th_dport);
+            size_payload = ntohs(ip->ip_len) - (size_ip + size_udp);
+            src_ip = ntohl(ip->ip_src.s_addr);
+            dest_ip = ntohl(ip->ip_dst.s_addr);
+            src_port = ntohs(udp->uh_sport);
+            dest_port = ntohs(udp->uh_dport);
+        }
+
+        ThreadKey key = { src_ip, dest_ip, src_port, dest_port };
+
+        // Checking for the existence of a thread with a given key (IPs, ports)
+        auto it = thread.find(key);
+        if (it == thread.end()) {
+            Thread new_thread;
+            new_thread.packets.push_back(packet);
+            new_thread.statistics.packet_count = 1;
+            new_thread.statistics.total_bytes = size_payload;
+
+            thread[key] = new_thread;
+        }
+        else {
+            it->second.packets.push_back(packet);
+            it->second.statistics.packet_count++;
+            it->second.statistics.total_bytes += size_payload;
+        }
     }
-    else if (ip->ip_p == IPPROTO_UDP) {
-        udp = (struct sniff_udp*)(packet + SIZE_ETHERNET + size_ip);
-        size_udp = sizeof(struct sniff_udp);
-
-        size_payload = ntohs(ip->ip_len) - (size_ip + size_udp);
-        src_ip = ntohl(ip->ip_src.s_addr);
-        dest_ip = ntohl(ip->ip_dst.s_addr);
-        src_port = ntohs(udp->uh_sport);
-        dest_port = ntohs(udp->uh_dport);
-    }
-
-    ThreadKey key = { src_ip, dest_ip, src_port, dest_port };
-
-    // Checking for the existence of a thread with a given key (IPs, ports)
-    auto it = thread.find(key);
-    if (it == thread.end()) {
-        Thread new_thread;
-        new_thread.packets.push_back(packet);
-        new_thread.statistics.packet_count = 1;
-        new_thread.statistics.total_bytes = size_payload;
-
-        thread[key] = new_thread;
-    }
-    else {
-        it->second.packets.push_back(packet);
-        it->second.statistics.packet_count++;
-        it->second.statistics.total_bytes += size_payload;
+    catch (const exception& e) {
+        cerr << "Packet handling error: " << e.what() << endl;
     }
 }
 
 void PcapHandler::process_file(const char* pcap_filename, const char* csv_filename, const char* max_packets) {
-    char errbuf[PCAP_ERRBUF_SIZE];
-    pcap_t* handle = pcap_open_offline(pcap_filename, errbuf);
-
-    if (handle == nullptr) {
-        cerr << "Couldn't open file: " << errbuf << endl;
-        return;
-    }
-
-    process_packets_internal(handle, csv_filename, max_packets);
-    pcap_close(handle);
-}
-
-void PcapHandler::process_interface(const char* dev, const char* csv_filename, const char* max_packets) {
-    char errbuf[PCAP_ERRBUF_SIZE];
-    pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
-
-    if (handle == nullptr) {
-        cerr << "Couldn't open interface: " << errbuf << endl;
-        const char* another_dev = pcap_lookupdev(errbuf);
-        if (another_dev != nullptr) {
-            cerr << "Trying default interface: " << another_dev << endl;
-            handle = pcap_open_live(another_dev, BUFSIZ, 1, 1000, errbuf);
-        }
+    try {
+        char errbuf[PCAP_ERRBUF_SIZE];
+        pcap_t* handle = pcap_open_offline(pcap_filename, errbuf);
 
         if (handle == nullptr) {
-            cerr << "Couldn't open default interface: " << errbuf << endl;
-            return;
+            throw runtime_error(string("Couldn't open file: ") + errbuf);
         }
-    }
 
-    process_packets_internal(handle, csv_filename, max_packets);
-    pcap_close(handle);
+        process_packets_internal(handle, csv_filename, max_packets);
+        pcap_close(handle);
+    }
+    catch (const exception& e) {
+        cerr << "File processing error: " << e.what() << endl;
+    }
+}
+void PcapHandler::process_interface(const char* dev, const char* csv_filename, const char* max_packets) {
+    try {
+        char errbuf[PCAP_ERRBUF_SIZE];
+        pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+
+        if (handle == nullptr) {
+            throw runtime_error(string("Couldn't open interface: ") + errbuf);
+        }
+
+        process_packets_internal(handle, csv_filename, max_packets);
+        pcap_close(handle);
+    }
+    catch (const exception& e) {
+        cerr << "Interface processing error: " << e.what() << endl;
+    }
 }
 
 void PcapHandler::process_packets(const char* source, const char* csv_filename, const char* max_packets) {
-    if (source_is_file(source)) {
-        process_file(source, csv_filename, max_packets);
+    try {
+        if (source_is_file(source)) {
+            process_file(source, csv_filename, max_packets);
+        }
+        else if (source_is_interface(source)) {
+            process_interface(source, csv_filename, max_packets);
+        }
+        else {
+            throw invalid_argument("Invalid source specified: " + string(source));
+        }
     }
-    else if (source_is_interface(source)) {
-        process_interface(source, csv_filename, max_packets);
-    }
-    else {
-        cerr << "Invalid source specified: " << source << endl;
+    catch (const exception& e) {
+        cerr << "Packet processing error: " << e.what() << endl;
     }
 }
 
 // The opening of the file and extraction of packets from it occur here.
 void PcapHandler::process_packets_internal(pcap_t* handle, const char* csv_filename, const char* max_packets) {
-    const char* filter_exp = "ip and (tcp or udp)";
-    struct bpf_program fp;
+    try {
+        const char* filter_exp = "ip and (tcp or udp)";
+        struct bpf_program fp;
 
-    if (pcap_compile(handle, &fp, filter_exp, 0, PCAP_NETMASK_UNKNOWN) == -1) {
-        cerr << "Couldn't parse filter " << filter_exp << ": " << pcap_geterr(handle) << endl;
-        pcap_close(handle);
-        return;
-    }
+        if (pcap_compile(handle, &fp, filter_exp, 0, PCAP_NETMASK_UNKNOWN) == -1) {
+            throw runtime_error(string("Couldn't parse filter ") + filter_exp + ": " + pcap_geterr(handle));
+        }
 
-    if (pcap_setfilter(handle, &fp) == -1) {
-        cerr << "Couldn't install filter " << filter_exp << ": " << pcap_geterr(handle) << endl;
+        if (pcap_setfilter(handle, &fp) == -1) {
+            throw runtime_error(string("Couldn't install filter ") + filter_exp + ": " + pcap_geterr(handle));
+        }
+
+        int cnt_of_packets = stoi(max_packets);
+        pcap_loop(handle, cnt_of_packets, packet_handler, nullptr);
+
         pcap_freecode(&fp);
-        pcap_close(handle);
-        return;
+
+        write_to_csv(thread, csv_filename);
     }
-
-    int cnt_of_packets = stoi(max_packets);
-    pcap_loop(handle, cnt_of_packets, packet_handler, nullptr);
-
-    pcap_freecode(&fp);
-
-    write_to_csv(thread, csv_filename);
+    catch (const exception& e) {
+        cerr << "Internal packet processing error: " << e.what() << endl;
+    }
 }
 
 bool PcapHandler::source_is_file(const char* file_name) {
@@ -153,20 +160,24 @@ bool PcapHandler::source_is_interface(const char* dev) {
 
 // The writing to the file takes plase in the format <Source IP> <Destination IP> <Source Port> <Destination Port> <Packet Count> <Total Bytes>
 void PcapHandler::write_to_csv(const unordered_map<ThreadKey, Thread, ThreadKeyHash>& threads, const string& filename) {
-    ofstream csv_file(filename);
+    try {
+        ofstream csv_file (filename);
 
-    if (!csv_file.is_open()) {
-        cerr << "CSV file is not creating" << endl;
-        return;
+        if (!csv_file.is_open()) {
+            throw runtime_error("CSV file is not creating");
+        }
+
+        csv_file << "Source IP,Destination IP,Source Port,Destination Port,Packet Count,Total Bytes" << endl;
+
+        for (const auto& entry : threads) {
+            const ThreadKey& key = entry.first;
+            const Thread& thread = entry.second;
+            csv_file << key.to_string() << "," << thread.statistics.packet_count << "," << thread.statistics.total_bytes << endl;
+        }
+
+        csv_file.close();
     }
-
-    csv_file << "Source IP,Destination IP,Source Port,Destination Port,Packet Count,Total Bytes" << endl;
-
-    for (const auto& entry : threads) {
-        const ThreadKey& key = entry.first;
-        const Thread& thread = entry.second;
-        csv_file << key.to_string() << "," << thread.statistics.packet_count << "," << thread.statistics.total_bytes << endl;
+    catch (const exception& e) {
+        cerr << "CSV writing error: " << e.what() << endl;
     }
-
-    csv_file.close();
 }
